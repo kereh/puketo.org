4e90e46974966ec565b763466147f005d196d404
Ronaldo Kereh (RK) <kreh.whoami@gmail.com>
2023-11-06 22:02:20 +0800

    update: fix padding top and bottom

--- app/berita/page.tsx
+++ app/berita/page.tsx
@@ -4,7 +4,7 @@ import BeritaCard from "@/components/berita/berita-card";
 export default async function page() {
   const berita = await getAllPosts();
   return (
-    <div className="px-6 lg:px-8 h-screen w-full py-6 space-y-3">
+    <div className="px-6 lg:px-8 h-screen w-full py-8 space-y-3">
       <h1 className="text-lg lg:text-xl font-semibold">Daftar Berita</h1>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         {berita.map((b, index) => (
